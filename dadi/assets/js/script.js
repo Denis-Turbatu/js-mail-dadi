7a8d99d672a80b1f3fde54fd635728f3a7b0f91e
Denis-Turbatu <101722351+Denis-Turbatu@users.noreply.github.com>
2024-03-28 17:50:26 +0100

    feat: add content from previous files on local repo to html, css and js files

---
+++ dadi/assets/js/script.js
@@ -0,0 +1,26 @@
+//INPUT (non necessari in quanto i numeri sono random)
+
+//ESECUZIONE LOGICA
+let numUser = Math.floor(Math.random() * 6) + 1;
+let numPc = Math.floor(Math.random() * 6) + 1;
+
+const userCardElem = document.querySelector('.card-user');
+const pcCardElem = document.querySelector('.card-pc');
+
+const userResultElem = document.getElementById('user-result');
+const pcResultElem = document.getElementById('pc-result');
+const winnerElem = document.getElementById('winner');
+
+userResultElem.innerHTML = numUser;
+pcResultElem.innerHTML = numPc;
+winnerElem.innerHTML = 
+console.log(numUser, "utente");
+console.log(numPc, "pc");
+
+if (numUser > numPc){
+    winnerElem.innerHTML = "Il vincitore sei TU!";
+}else if(numUser < numPc){
+    winnerElem.innerHTML = "Il vincitore è il Computer";
+}else{
+    winnerElem.innerHTML = "La partita è in pareggio";
+}
