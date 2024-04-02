64383195ee99601480fb04fe0607501643f743ec
Denis-Turbatu <turbatudenis81@gmail.com>
2024-04-02 09:59:13 +0200

    fix: js and css in mail exercise for merge

--- mail/assets/js/script.js
+++ mail/assets/js/script.js
@@ -2,30 +2,17 @@
 const emailVerified = [
   "pippo@gmail.com",
   "pluto@gmail.com",
-<<<<<<< HEAD
   "paperino@gmail.com"
-=======
-  "paperino@gmail.com",
->>>>>>> 4cb8ff7d04109f17ff1c67973e92947e08b48671
 ];
 
 // ESECUZIONE LOGICA
 
-<<<<<<< HEAD
 const btnSubmit = document.getElementById("submit");
 const messageVerify = document.querySelector(".email-message");
 const verifyEmail = document.getElementById("user-email").value;
 // const emailResult = document.querySelector('user-email');
 
 btnSubmit.addEventListener("click", function () {
-=======
-const btnSubmit = document.getElementById('submit');
-const messageVerify = document.querySelector('.email-message');
-const verifyEmail = document.getElementById('user-email').value;
-// const emailResult = document.querySelector('user-email');
-
-btnSubmit.addEventListener('click', function() {
->>>>>>> 4cb8ff7d04109f17ff1c67973e92947e08b48671
   let trovato = false;
 
   //scorriamo tutto l'array per vedere il contenuto
@@ -36,10 +23,6 @@ btnSubmit.addEventListener('click', function() {
     }
   }
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 4cb8ff7d04109f17ff1c67973e92947e08b48671
   if (trovato) {
     // emailResult.classList.add = 'outline-green';
     // messageVerify.classList.add = ' green';
@@ -49,8 +32,4 @@ btnSubmit.addEventListener('click', function() {
     // messageVerify.classList.add = ' red';
     messageVerify.innerHTML = "Mi dispiace! La tua email non è verificata";
   }
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> 4cb8ff7d04109f17ff1c67973e92947e08b48671
